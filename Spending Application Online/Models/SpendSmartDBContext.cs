2afe23c4d50d7fbcfdd668dd1f1784830912c453
kvau146 <kvau146@wgu.edu>
2024-07-18 19:13:46 -0400

    All my files

---
+++ Spending Application Online/Models/SpendSmartDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpendSmart.Models;
+
+namespace Spending_Application_Online.Models
+{
+    public class SpendSmartDBContext :DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public SpendSmartDBContext(DbContextOptions<SpendSmartDBContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
